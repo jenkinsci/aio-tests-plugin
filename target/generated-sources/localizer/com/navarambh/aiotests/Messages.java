93bbe9b5df05fd9a065e1a11a8df98f298a019ac
Bruno Verachten <gounthar@gmail.com>
2024-08-20 13:32:18 +0000

    Applied transformations with specified recipes

---
+++ target/generated-sources/localizer/com/navarambh/aiotests/Messages.java
@@ -0,0 +1,27 @@
+// CHECKSTYLE:OFF
+
+package com.navarambh.aiotests;
+
+import org.jvnet.localizer.ResourceBundleHolder;
+import org.kohsuke.accmod.Restricted;
+
+
+/**
+ * Generated localization support class.
+ * 
+ */
+@SuppressWarnings({
+    "",
+    "PMD",
+    "all"
+})
+@Restricted(org.kohsuke.accmod.restrictions.NoExternalUse.class)
+public class Messages {
+
+    /**
+     * The resource bundle reference
+     * 
+     */
+    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
+
+}
